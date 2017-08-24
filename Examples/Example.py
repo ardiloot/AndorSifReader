2f0e00806ebef73c663946b250f4eab22dcb6949
Ardi <ardi.loot@ut.ee>
2017-08-24 15:09:19 +0300

    Improved example
    - Added example .sif file
    - Example prints all properties and plots both signal and bg spectrums

--- Examples/Example.py
+++ Examples/Example.py
@@ -1,4 +1,27 @@
+import pylab as plt
 from AndorSifReader import AndorSifFile
 
 if __name__ == "__main__":
-    sif = AndorSifFile(r"C:\Users\Ardi\Dropbox\LABOR\Projektid\Crystalsol\eksperiment\220817\led spekter.sif")
+    # Read file
+    sif = AndorSifFile(r"led spectrum.sif")
+    
+    # Print properties
+    print("\n".join(["%s: %s" % (k, v) for k, v in sif.signal.props.items()]))
+    
+    # Plot LED spectrum
+    plt.figure(figsize = (12, 6))
+    
+    plt.subplot(121)
+    plt.plot(1e9 * sif.signal.wls, sif.signal.data)
+    plt.title("Signal")
+    plt.xlabel("Wavelength (nm)")
+    plt.ylabel("Signal (a.u)")
+    
+    plt.subplot(122)
+    plt.plot(1e9 * sif.bg.wls, sif.bg.data)
+    plt.title("Background")
+    plt.xlabel("Wavelength (nm)")
+    plt.ylabel("Signal (a.u)")
+    
+    plt.tight_layout()
+    plt.show()
